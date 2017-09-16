33b963af5e6bda15c4429b484c7af3983e00990f
Ierofantis <evil69669@gmail.com>
2017-09-16 17:40:57 +0300

    Refactoring and Functionality

--- qc/app/scripts/services/mainService.js
+++ qc/app/scripts/services/mainService.js
@@ -1,13 +1,25 @@
 'use strict';
+/**
+ * @ngdoc function
+ * @name qcApp.service:mainServiceObj
+ * @description
+ * # mainServiceObj
+ * Service of the qcApp
+ */
 
 angular.module('qcApp')
 .service('mainServiceObj', function($http) {
 
     return {
 
-      getStudents: function() {
-        var path = ''
+      getCoordinates: function(lat, long) {
+        var path = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+long+'&key=AIzaSyCHf2EeOg7T167gSkNU_ljDPetPXysSMFg'
           return $http.get(path) ;        
+      },
+
+      getQuality: function(place){
+      	 var path = 'https://maps.googleapis.com/maps/api/place/details/json?placeid='+place+'&key=AIzaSyCHf2EeOg7T167gSkNU_ljDPetPXysSMFg'
+          return $http.get(path) ;  
       }
     };
   });
